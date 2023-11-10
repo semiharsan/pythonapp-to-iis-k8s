f172812d2403556e0d91119e04c3f974fc4a8835
semiharsan <40902660+semiharsan@users.noreply.github.com>
2023-11-10 11:04:11 +0000

    Update hello.py

--- PythonAppToIIS/app/hello.py
+++ PythonAppToIIS/app/hello.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Congratulations, You have deployed Python Application on Windows IIS Server "
+    return "Congratulations, You have deployed Python Application on Windows IIS Server and Kubernetes Cluster. You are very good at Devops. "
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, debug=True)
